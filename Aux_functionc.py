61af239d711b5d0fcdedc17e4e4df7029f272a63
AlexDruzina <alexdruzina@gmail.com>
2022-03-05 17:20:06 +0200

    added - aux. functions to plot the output of the models

--- Aux_functionc.py
+++ Aux_functionc.py
@@ -1,26 +1,7 @@
+import pickle
 import numpy as np
-import torch.nn.functional as F
 from matplotlib import ticker
 import matplotlib.pyplot as plt
-
-
-def reconstruction_loss(x, x_recon):
-    batch_size = x.size(0)
-    x_recon = F.sigmoid(x_recon)
-    recon_loss = F.mse_loss(x_recon, x, size_average=False).div(batch_size)
-    return recon_loss
-
-
-def kl_divergence(mu, logvar):
-    batch_size = mu.size(0)
-    assert batch_size != 0
-    if mu.data.ndimension() == 4:
-        mu = mu.view(mu.size(0), mu.size(1))
-    if logvar.data.ndimension() == 4:
-        logvar = logvar.view(logvar.size(0), logvar.size(1))
-    klds = -0.5 * (1 + logvar - mu.pow(2) - logvar.exp())
-    total_kld = klds.sum(1).mean(0, True)
-    return total_kld
 
 
 def imshow(img, title):
@@ -43,4 +24,40 @@ def plot_figure(x, y, x_title, y_title, fig_title):
     plt.grid()
     plt.tight_layout()
     fig.savefig(fig_title, bbox_inches='tight', dpi=600)
+    plt.show()
+
+
+def plot_output(files, output_type):
+    for file_name in files:
+        file = open(file_name, 'rb')
+        object_file = pickle.load(file)
+        file.close()
+        x = [i for i in range(len(object_file[output_type]))]
+        y = object_file[output_type]
+        from scipy.ndimage.filters import gaussian_filter1d
+        ysmoothed = gaussian_filter1d(y, sigma=2)
+        plt.plot(x, ysmoothed, label=file_name)
+
+
+def plot_figure(files, output_type):
+    fig, ax = plt.subplots()
+    plot_output(files, output_type)
+    plt.tick_params(labelsize=15)
+    plt.xlabel('Training Steps', fontsize=15)
+    if output_type == 'kl_list':
+        title = 'KL Divergence'
+        limit = [0, 300]
+        plt.axhline(y=170, color='black')
+        plt.axhline(y=200, color='black')
+    elif output_type == 'recon_loss_list':
+        title = 'Reconstruction Loss'
+        limit = [0, 4000]
+    plt.ylabel(title, fontsize=15)
+    plt.ylim(limit)
+    plt.xlim(0, 6000)
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{}'.format(int(x))))
+    plt.legend(loc='best', prop={'size': 11.5})
+    plt.grid()
+    plt.tight_layout()
+    fig.savefig(output_type, bbox_inches='tight', dpi=600)
     plt.show()
