aa3823c0ec9bdf189d0642af2eba9d507baa44ab
AlexDruzina <alexdruzina@gmail.com>
2022-03-05 21:43:53 +0200

    create vizualize_output.py to plot and save model's output graphs

---
+++ Image_Generation_celebA/vizualize_output.py
@@ -0,0 +1,17 @@
+from Aux_functionc import plot_figure
+
+
+def main():
+    files = [
+        'celebA_output_betaVAE_100',
+        'celebA_output_controlVAE_170',
+        'celebA_output_controlVAE_180',
+        'celebA_output_controlVAE_200',
+        'celebA_output_VAE_0'
+    ]
+    plot_figure(files, 'recon_loss_list')
+    plot_figure(files, 'kl_list')
+
+
+if __name__ == '__main__':
+    main()
