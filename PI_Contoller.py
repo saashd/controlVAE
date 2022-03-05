78e8799de260bfd05203c74a8fa65563727250ef
AlexDruzina <alexdruzina@gmail.com>
2022-03-05 17:15:12 +0200

    fix - save data from prev. iteration to controller state

--- PI_Contoller.py
+++ PI_Contoller.py
@@ -1,25 +1,29 @@
 from math import exp
 
 
-def pi_controller(desired_KL, KL, beta_max, beta_min, N, Kp=0.001, Ki=-0.001):
-    I_p0 = 0
-    beta0 = 0
-    for i in range(0, N):
-        error = desired_KL - KL
-        P_pi = Kp / (1.0 + float(exp(error)))
-        if beta_min <= beta0 <= beta_max:
-            I_pi = I_p0 - Ki * error
-        else:
-            I_pi = I_p0  # Anti-windup
+class PI_Controller():
+    def __init__(self):
+        self.I_p = 0
+        self.beta = 0
+        self.error = 0
 
-        beta = P_pi + I_pi + beta_min
+    def pi(self, desired_KL, KL, beta_max=1, beta_min=0, N=1, Kp=0.01, Ki=0.0001):
+        beta_i = None
+        for i in range(0, N):
+            error_i = desired_KL - KL
+            P_i = Kp / (1.0 + exp(error_i))
+            I_i = self.I_p - Ki * error_i
+            if 0 > self.beta >= 1:
+                I_i = self.I_p  # Anti-windup
+            beta_i = P_i + I_i + beta_min
 
-        if beta > beta_max:
-            beta = beta_max
-        if beta < beta_min:
-            beta = beta_min
+            self.I_p = I_i
+            self.beta = beta_i
+            self.error = error_i
 
-        I_p0 = I_pi
-        beta0 = beta
+            if beta_i > beta_max:
+                beta_i = beta_max
+            if beta_i < beta_min:
+                beta_i = beta_min
 
-    return beta0
+        return beta_i
